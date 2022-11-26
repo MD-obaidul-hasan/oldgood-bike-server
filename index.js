4faf291b12990198c0594688a529d7a641871e10
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-11-26 13:46:57 +0600

    single data

--- index.js
+++ index.js
@@ -26,12 +26,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // });
 async function run() {
   try {
-    const oldgoodbikeOptionCollection = client.db('oldgoodbike').collection('catagoriesbike');
+    const oldgoodbikeOptionCollection = client.db('goodoldbike').collection('gooldbike');
+    const bookingCollection = client.db('goodoldbike').collection('bookings'); 
+    const usersCollection = client.db('goodoldbike').collection('users'); 
 
-    app.get('/catagoriesbike', async(req,res) => {
+    app.get('/gooldbike', async(req,res) => {
       const query = {};
       const options = await oldgoodbikeOptionCollection.find(query).toArray();
+    });
+
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
     })
+
+    app.post('/bookings', async(req, res) =>{
+      const booking = req.bodyconsole.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    } )
   }
   finally {
 
